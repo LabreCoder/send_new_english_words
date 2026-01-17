cc95df373cca4246a8e1e85ffb92f630ad58763a
LabreCoder <jvlabremachado@gmail.com>
2026-01-17 11:20:28 -0300

    Teste para um novo retorno

--- get_word.py
+++ get_word.py
@@ -1,8 +1,18 @@
 import requests
+import sys
 
 def get_random_word():
     url = "https://random-word-api.herokuapp.com/word"
     response = requests.get(url, timeout=10)
     response.raise_for_status()
+    '''
+    if response.status_code != 200 and response.status_code != 500:
+        return sys.exit(1)
+    
+    elif response.status_code == 500:
+        return sys.exit(1)
+    else: 
+        return response.json()[0]
+    '''
     return response.json()[0]
 
