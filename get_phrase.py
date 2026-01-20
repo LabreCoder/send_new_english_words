64fa10344700b2939bcfae1703a5d2b0d904e4a4
LabreCoder <jvlabremachado@gmail.com>
2026-01-20 10:32:42 -0300

    Adicionando alteração para as definições e exemplos

--- get_phrase.py
+++ get_phrase.py
@@ -4,22 +4,29 @@ def get_definition(word):
     url = f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
     response = requests.get(url, timeout=10)
 
-    if response.status_code != 200:
-        return "Meaning not found."
-
     results = []
+    dictionary_url = f"https://dictionary.cambridge.org/"
 
-    data = response.json()
+    if response.status_code != 200:
+        results.append((f"Definition not found. Please, check on: {dictionary_url}", f"No example available. Please, check on: {dictionary_url}"))
+        return results
 
-    for meaning in data[0]["meanings"]:
-        for definition_data in meaning["definitions"]:
-            definition = definition_data.get(
-                "definition", "No definition available."
-            )
-            example = definition_data.get(
-                "example", "No example available."
-            )
+    else:
 
-            results.append((definition, example))
-            
-    return results
+        data = response.json()
+
+        if data[0]["meanings"]:
+            for meaning in data[0]["meanings"]:
+                for definition_data in meaning["definitions"]:
+                    definition = definition_data.get(
+                        "definition", f"No definition available. Please, check on: {dictionary_url}"
+                    )
+                    example = definition_data.get(
+                        "example", f"No example available. Please, check on: {dictionary_url}"
+                    )
+
+                    results.append((definition, example))
+        else: 
+            results.append((f"Definition not found. Please, check on: {dictionary_url}", f"No example available. Please, check on: {dictionary_url}"))
+                
+        return results
